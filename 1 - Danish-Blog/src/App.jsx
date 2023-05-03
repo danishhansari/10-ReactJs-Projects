20cff79117835390b016ffeedad784d6b9c10527
Danish <dan71ish@gmail.com>
2023-05-04 03:19:34 +0530

    started working on projects

---
+++ 1 - Danish-Blog/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import HomePage from './HomePage'
+import Navbar from './Navbar'
+
+const App = () => { 
+  return (
+    <div className="App">
+      <Navbar /> 
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
---
+++ 1 - Danish-Blog/src/HomePage.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+const HomePage = () => {
+  return (
+    <div className='heading'>
+      <h1>HomePage</h1>
+      </div>
+  )
+}
+
+export default HomePage
